cd883713265399d42e38240ed2df50387fa1cec9
Marvin Eggerschwiler <marvin_eggerschwiler@sluz.ch>
2024-12-13 12:49:54 +0100

    4.1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,24 @@
 package main
 
-// TODO: implement the function computeGrade
+import (
+	"fmt"
+)
+
+func computeGrade(gotPoints, maxPoints float64) float64 {
+	if maxPoints <= 0 {
+		return 1.0
+	}
+	grade := 1.0 + (5.0 * gotPoints / maxPoints)
+	if grade > 6.0 {
+		return 6.0
+	} else if grade < 1.0 {
+		return 1.0
+	}
+	return grade
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 28.0)) // 4.125
+	fmt.Println(computeGrade(28.0, 28.0)) // 6.0
+	fmt.Println(computeGrade(0, 28.0))    // 1.0
 }
